801cbdd352d8a34abde0e5ad71707c3258f12229
dd86k <dd@dax.moe>
2022-12-28 11:50:31 -0500

    Move some constants as template parameters

--- source/sha3d.d
+++ source/sha3d.d
@@ -30,9 +30,12 @@ version (SHA3D_Trace)
 /// Params:
 ///   digestSize = Digest size in bits.
 ///   shake = SHAKE XOF digest size in bits. Defaults to 0 for SHA-3.
+///   sponge = Sponge size in bits, must be a power of 25. Defaults to 1600.
+///   rounds = Number of rounds for transformation function. Defaults to 24.
 ///
 /// Throws: No exceptions are thrown.
-public struct KECCAK(uint digestSize, uint shake = 0)
+public struct KECCAK(uint digestSize,
+    uint shake = 0, uint sponge = 1600, size_t rounds = 24)
 {
     version (SHA3D_Trace) {}
     else
@@ -40,32 +43,9 @@ public struct KECCAK(uint digestSize, uint shake = 0)
         @safe: @nogc: nothrow: pure:
     }
     
-    //TODO: Consider moving the definitions as template parameters
-    
-    /// Sponge size. Default is 1600 bits (200 Bytes).
-    /// Must be a power of 25 (25, 50, 100, 200, 400, 800, or 1600).
-    private enum SPONGE = 1600;
-    /// Number of rounds. 24 rounds for SHA-3 and SHAKE.
-    private enum ROUNDS = 24;
-    /// RC constants.
-    private immutable static ulong[ROUNDS] K_RC = [
-        0x0000000000000001, 0x0000000000008082, 0x800000000000808a, 0x8000000080008000,
-        0x000000000000808b, 0x0000000080000001, 0x8000000080008081, 0x8000000000008009,
-        0x000000000000008a, 0x0000000000000088, 0x0000000080008009, 0x000000008000000a,
-        0x000000008000808b, 0x800000000000008b, 0x8000000000008089, 0x8000000000008003,
-        0x8000000000008002, 0x8000000000000080, 0x000000000000800a, 0x800000008000000a,
-        0x8000000080008081, 0x8000000000008080, 0x0000000080000001, 0x8000000080008008
-    ];
-    /// Rho indexes.
-    private immutable static int[ROUNDS] K_RHO = [
-        1,  3,  6, 10, 15, 21, 28, 36, 45, 55,  2, 14,
-        27, 41, 56,  8, 25, 43, 62, 18, 39, 61, 20, 44
-    ];
-    /// PI indexes.
-    private immutable static size_t[ROUNDS] K_PI = [
-        10,  7, 11, 17, 18, 3,  5, 16,  8, 21, 24, 4,
-        15, 23, 19, 13, 12, 2, 20, 14, 22,  9,  6, 1
-    ];
+    static assert(sponge <= 1600,   "Sponge can't be over 1600 bits.");
+    static assert(sponge % 25 == 0, "Sponge must be a power of 25.");
+    static assert(rounds <= 24,     "Rounds can't be over 24.");
     
     static if (shake)
     {
@@ -85,8 +65,28 @@ public struct KECCAK(uint digestSize, uint shake = 0)
         private enum digestSizeBytes = digestSize / 8; /// Digest size in bytes
     }
     
+    /// RC constants.
+    private immutable static ulong[24] K_RC = [
+        0x0000000000000001, 0x0000000000008082, 0x800000000000808a, 0x8000000080008000,
+        0x000000000000808b, 0x0000000080000001, 0x8000000080008081, 0x8000000000008009,
+        0x000000000000008a, 0x0000000000000088, 0x0000000080008009, 0x000000008000000a,
+        0x000000008000808b, 0x800000000000008b, 0x8000000000008089, 0x8000000000008003,
+        0x8000000000008002, 0x8000000000000080, 0x000000000000800a, 0x800000008000000a,
+        0x8000000080008081, 0x8000000000008080, 0x0000000080000001, 0x8000000080008008
+    ];
+    /// Rho indexes.
+    private immutable static int[24] K_RHO = [
+        1,  3,  6, 10, 15, 21, 28, 36, 45, 55,  2, 14,
+        27, 41, 56,  8, 25, 43, 62, 18, 39, 61, 20, 44
+    ];
+    /// PI indexes.
+    private immutable static size_t[24] K_PI = [
+        10,  7, 11, 17, 18, 3,  5, 16,  8, 21, 24, 4,
+        15, 23, 19, 13, 12, 2, 20, 14, 22,  9,  6, 1
+    ];
+    
     /// Digest size in bits.
-    enum blockSize = (SPONGE - digestSize * 2); // Required for HMAC.
+    enum blockSize = (sponge - digestSize * 2); // Required for HMAC.
     
     //  ...0: Reserved
     //    01: SHA-3
@@ -220,7 +220,7 @@ private:
     {
         version (BigEndian) swap;
 
-        for (size_t round; round < ROUNDS; ++round)
+        for (size_t round; round < rounds; ++round)
         {
             // Theta
             THETA1(0); THETA1(1); THETA1(2); THETA1(3); THETA1(4);
